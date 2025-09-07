0616a7341f50ac31467b44f344be0f670299e68d
dodo4545 <dominique_clerge@yahoo.com>
2025-09-07 12:41:42 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,21 @@
-alert("Hello world");
-let favoriteFood = 26;
-document.write(favoriteFood);
+// Create a new variable called pokemonList and assign to it a blank array
+let pokemonList = [];
+
+// Add Pokémon objects to the array
+pokemonList.push({
+   name: "Bulbasaur",
+   height: 7,
+   types: ['grass', 'poison']
+});
+
+pokemonList.push({
+   name: "Charmander",
+   height: 6,
+   types: ['fire']
+});
+
+pokemonList.push({
+   name: "Squirtle",
+   height: 5,
+   types: ['water']
+});
