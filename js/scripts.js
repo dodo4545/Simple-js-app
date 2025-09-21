07b44adc5375b71d60eb3f41c863c75f59f80b3e
dodo4545 <dominique_clerge@yahoo.com>
2025-09-21 13:03:33 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,64 @@
-const pokemonList = [
-  {
-    name: "Bulbasaur",
-    height: 7,
-    types: ["grass", "poison"],
-  },
-  {
-    name: "Charizard",
-    height: 17,
-    types: ["fire", "flying"],
-  },
-  {
-    name: "Squirtle",
-    height: 5,
-    types: ["water"],
-  },
-];
+// IIFE to encapsulate the pokemonList and expose functions
+const pokemonRepository = (function () {
+  // Private array to store Pokémon
+  let pokemonList = [
+    {
+      name: "Bulbasaur",
+      height: 7,
+      types: ["grass", "poison"],
+    },
+    {
+      name: "Charizard",
+      height: 17,
+      types: ["fire", "flying"],
+    },
+    {
+      name: "Squirtle",
+      height: 5,
+      types: ["water"],
+    },
+  ];
 
+  // Public function to return all Pokémon
+  function getAll() {
+    return pokemonList;
+  }
 
-// Loop through each Pokémon in the pokemonList array
-for (let i = 0; i < pokemonList.length; i++) {
-  const pokemon = pokemonList[i]; // Get the current Pokémon object
-  let output = `${pokemon.name} (height: ${pokemon.height})`; // Create the output string
+  // Public function to add a Pokémon
+  function add(item) {
+    if (typeof item === "object" && "name" in item) {
+      pokemonList.push(item);
+    } else {
+      console.log("Invalid Pokémon item");
+    }
+  }
+
+  // Expose public methods
+  return {
+    getAll: getAll,
+    add: add,
+  };
+})();
+
+// Using forEach() to iterate over each Pokémon
+pokemonRepository.getAll().forEach(function (pokemon) {
+  let output = `${pokemon.name} (height: ${pokemon.height})`;
 
   // Conditional to check if the height is above a certain value
   if (pokemon.height > 10) {
-    // You can change the value 10 to whatever you prefer
-    output += " - Wow, that’s big!"; // Add the special note if the condition is met
+    output += " - Wow, that’s big!";
   }
+
+  // Write the output to the DOM
+  document.write(output + "<br>"); // Use <br> for line breaks
+});
+
+// Example of adding a new Pokémon
+pokemonRepository.add({
+  name: "Pikachu",
+  height: 4,
+  types: ["electric"],
+});
 
   // Write the output to the DOM
   document.write(output + "<br>"); // Use <br> for line breaks
